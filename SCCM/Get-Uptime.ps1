e13978845adc1135fe271405212a735335ea12c1
Fifteen 15 Studios <havens1515@gmail.com>
2018-07-17 15:11:23 -0400

    Trim output
    Trim empty white space from output

--- SCCM/Get-Uptime.ps1
+++ SCCM/Get-Uptime.ps1
@@ -3,7 +3,7 @@ $uptime = ((get-date) - (gcim Win32_OperatingSystem).LastBootUpTime)
 $Output = ""
 
 if($uptime.Days -gt 0)
-{$Output += $uptime.Days.ToString() + " Days " }
+{$Output += $uptime.Days.ToString() + " Days "}
 
 if($uptime.Hours -gt 0)
 {$Output += $uptime.Hours.ToString() + " Hours "}
@@ -12,6 +12,6 @@ if($uptime.Minutes -gt 0)
 {$Output += $uptime.Minutes.ToString() + " Minutes "}
 
 if($uptime.Seconds -gt 0)
-{$Output += $uptime.Seconds.ToString() + " Seconds"}
+{$Output += $uptime.Seconds.ToString() + " Seconds "}
 
-"Uptime: $Output"
+"Uptime: $($Output.trim())"
