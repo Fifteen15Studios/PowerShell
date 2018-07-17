e95b459dfd2d4fad70307572fbb00172cb82f1cd
Fifteen 15 Studios <havens1515@gmail.com>
2018-07-17 15:32:41 -0400

    Create Set-WOL.ps1

---
+++ SCCM/Task Sequence Scripts/Set-WOL.ps1
@@ -0,0 +1,27 @@
+# Turns on Wake on LAN in HP computers.
+#
+# I noticed that the options for HP WoL BIOS Settings were not always named the
+# same way, but that the option that we want is always the last option. This
+# is sometimes called "boot to disk" or "follow boot order" but it was always
+# the last in the list. 
+#
+# This script uses the HP BIOS Config Utility
+# (http://ftp.hp.com/pub/caps-softpaq/cmit/HP_BCU.html)
+# to set the WoL setting to be the last option in the list.
+#
+# This must be used as part of a package, and that package should point to
+# the files for the BCU in order to work.
+
+$Setting = "Wake On LAN"
+
+$instance = get-ciminstance -classname hp_biossetting -namespace "root\hp\instrumentedbios" | where name -eq $Setting
+$choice = ($instance.possiblevalues)[$instance.possiblevalues.count-1]
+
+if((Get-WmiObject Win32_OperatingSystem).OSArchitecture -eq "64-bit")
+{
+    .\BiosConfigUtility64.exe /setvalue:"$Setting","$choice"
+}
+else
+{
+    .\BiosConfigUtility.exe /setvalue:"$Setting","$choice"
+}
