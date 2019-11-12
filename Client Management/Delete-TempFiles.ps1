d65e31a5875f3cae603015ffbd5cd863d644301c
Fifteen 15 Studios <havens1515@gmail.com>
2019-11-12 14:20:12 -0500

    Fix weird double quotes

--- Client Management/Delete-TempFiles.ps1
+++ Client Management/Delete-TempFiles.ps1
@@ -3,9 +3,9 @@
 # Deletes files from various locations used for temporary storage
 
 $tempFolders = @( 
-“C:\Windows\Temp\*”, # Default Windows temp location
-“C:\Documents and Settings\*\Local Settings\temp\*”, # Windows XP user temp
-“C:\Users\*\Appdata\Local\Temp\*”, # Windows Vista+ user temp
+"C:\Windows\Temp\*", # Default Windows temp location
+"C:\Documents and Settings\*\Local Settings\temp\*", # Windows XP user temp
+"C:\Users\*\Appdata\Local\Temp\*", # Windows Vista+ user temp
 "C:\windows\ccmcache\*" # Default SCCM temp location
 )
 
