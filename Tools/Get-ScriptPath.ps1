c7af5fd3c80f7526f2469ddcf30728405f57b55a
Fifteen 15 Studios <havens1515@gmail.com>
2019-11-12 09:42:47 -0500

    Add Get-ScriptPath.ps1

---
+++ Tools/Get-ScriptPath.ps1
@@ -0,0 +1,28 @@
+﻿###############################################################################
+#
+# This function returns the path of the running script, regardless of what
+# PowerShell environment you are in.
+#
+###############################################################################
+
+function Get-ScriptPath()
+{
+    # If using PowerShell ISE
+    if ($psISE)
+    {
+        $ScriptPath = Split-Path -Parent -Path $psISE.CurrentFile.FullPath
+    }
+    # If using PowerShell 3.0 or greater
+    elseif($PSVersionTable.PSVersion.Major -gt 3)
+    {
+        $ScriptPath = $PSScriptRoot
+    }
+    # If using PowerShell 2.0 or lower
+    else
+    {
+        $ScriptPath = split-path -parent $MyInvocation.MyCommand.Path
+    }
+
+    # Return result
+    $ScriptPath
+}
