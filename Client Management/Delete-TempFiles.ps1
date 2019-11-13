c1b70f6d95ea7df2a151d13f36664103071f9c97
Fifteen 15 Studios <havens1515@gmail.com>
2019-11-13 09:08:15 -0500

    Update Delete-TempFiles.ps1

--- Client Management/Delete-TempFiles.ps1
+++ Client Management/Delete-TempFiles.ps1
@@ -1,12 +1,29 @@
-#Requires -runasadministrator
+###############################################################################
 #
 # Deletes files from various locations used for temporary storage
+#
+###############################################################################
 
-$tempFolders = @( 
-"C:\Windows\Temp\*", # Default Windows temp location
-"C:\Documents and Settings\*\Local Settings\temp\*", # Windows XP user temp
-"C:\Users\*\Appdata\Local\Temp\*", # Windows Vista+ user temp
-"C:\windows\ccmcache\*" # Default SCCM temp location
-)
+#Requires -runasadministrator
+function Delete-TempFiles()
+{
+    $OSVersion = [environment]::OSVersion.Version
 
-Remove-Item $tempFolders -recurse -force -ErrorAction SilentlyContinue
+    $tempFolders = @(
+    "C:\Windows\Temp\*", # Default Windows temp location
+    "C:\windows\ccmcache\*" # Default SCCM temp location
+    )
+
+    # Windows XP user temp
+    if($OSVersion.Major -eq 5 -and $OSVersion.Minor -ge 1)
+    {
+        $tempFolders += "C:\Documents and Settings\*\Local Settings\temp\*"
+    }
+    # Windows Vista+ user temp
+    elseif($OSVersion.Major -ge 6)
+    {
+        $tempFolders += "C:\Users\*\Appdata\Local\Temp\*"
+    }
+
+    Remove-Item $tempFolders -recurse -force -ErrorAction SilentlyContinue
+}
