350311b4d0d6f0c7302afbf7aa6e92fcc3490844
Fifteen 15 Studios <havens1515@gmail.com>
2019-11-21 13:22:55 -0500

    Update Get-Uptime.ps1

--- Client Management/Get-Uptime.ps1
+++ Client Management/Get-Uptime.ps1
@@ -2,16 +2,25 @@ $uptime = ((get-date) - (gcim Win32_OperatingSystem).LastBootUpTime)
 
 $Output = ""
 
-if($uptime.Days -gt 0)
+if($uptime.Days -gt 1)
 {$Output += $uptime.Days.ToString() + " Days "}
+elseif($uptime.Days -eq 1)
+{$Output += "1 Day "}
 
-if($uptime.Hours -gt 0)
+
+if($uptime.Hours -gt 1)
 {$Output += $uptime.Hours.ToString() + " Hours "}
+elseif($uptime.Hours -eq 1)
+{$Output += "1 Hour "}
 
-if($uptime.Minutes -gt 0) 
+if($uptime.Minutes -gt 1) 
 {$Output += $uptime.Minutes.ToString() + " Minutes "}
+elseif($uptime.Minutes -eq 1) 
+{$Output += "1 Minute "}
 
-if($uptime.Seconds -gt 0)
+if($uptime.Seconds -gt 1)
 {$Output += $uptime.Seconds.ToString() + " Seconds "}
+elseif($uptime.Seconds -eq 1)
+{$Output += "1 Second "}
 
 "Uptime: $($Output.trim())"
