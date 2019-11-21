1ddb99da6bf5853b3dc7386372591b12fc6bc868
Fifteen 15 Studios <havens1515@gmail.com>
2019-11-21 15:59:36 -0500

    Update Get-WindowsInfo.ps1

--- Client Management/Get-WindowsInfo.ps1
+++ Client Management/Get-WindowsInfo.ps1
@@ -9,10 +9,11 @@ $Name = (Get-WmiObject Win32_OperatingSystem).caption
 $ServerOS = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").InstallationType -eq "Server"
 $Bit = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
 $Version = [Version]([System.Diagnostics.FileVersionInfo]::GetVersionInfo("$env:SystemRoot\System32\kernel32.dll").ProductVersion)
-# If Windows 10, get Release ID
-if($Version.Major -ge 10){$ReleaseID=(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId}
 $Build= (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentBuild
 $InstallDate = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970')).AddSeconds($(get-itemproperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion").InstallDate)
+
+# If Windows 10, get Release ID
+if($Version.Major -ge 10){$ReleaseID=(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId}
 
 function getUptime
 {
@@ -24,7 +25,6 @@ function getUptime
     {$Output += $uptime.Days.ToString() + " Days "}
     elseif($uptime.Days -eq 1)
     {$Output += "1 Day "}
-
 
     if($uptime.Hours -gt 1)
     {$Output += $uptime.Hours.ToString() + " Hours "}
@@ -47,9 +47,9 @@ function getUptime
 # Put Information into an object
 $Info = New-Object -TypeName psobject
 
-$Info | Add-Member -MemberType NoteProperty -Name OSName -Value $Name
+$Info | Add-Member -MemberType NoteProperty -Name Name -Value $Name
 $Info | Add-Member -MemberType NoteProperty -Name Bits -Value $Bit
-$Info | Add-Member -MemberType NoteProperty -Name ServerOS -Value $ServerOS
+$Info | Add-Member -MemberType NoteProperty -Name Server -Value $ServerOS
 $Info | Add-Member -MemberType NoteProperty -Name Version -Value $Version
 # If Windows 10, Add Release ID
 if($Version.Major -ge 10){$Info | Add-Member -MemberType NoteProperty -Name ReleaseID -Value $ReleaseID}
