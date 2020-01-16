e573570e67da6d4496cfcf26cf156a788172336f
Fifteen 15 Studios <havens1515@gmail.com>
2020-01-16 14:10:33 -0500

    Update Remove-Software.ps1

--- Client Management/Remove-Software.ps1
+++ Client Management/Remove-Software.ps1
@@ -28,12 +28,12 @@ function uninstall($App)
     $ID = $App.IdentifyingNumber
     $Version = $App.Version
     $Answer = Read-Host "Would you like to uninstall" $ActualName $Version"? (Y/[N])"
+
     if($Answer -ne "" -and $Answer.ToLower()[0] -eq "y")
     {
         "Uninstalling $ActualName silently..."
         Start-Process msiexec -ArgumentList "/qn /x $ID" -Wait
     }
-
 }
 
 # If there is no macth found
@@ -42,18 +42,16 @@ if($Software -eq $null)
     "$SoftwareToFind is not installed"
 }
 # If there is more than 1 matching application
-elseif($Software.count -gt 1)
+else
 {
-    "Found $($Software.count) matching applications:"
-    $Software | ft
+    if($Software.count -gt 1)
+    {
+        "Found $($Software.count) matching applications:"
+        $Software | ft
+    }
 
     foreach($App in $Software)
     {
         uninstall($App)
     }
-}
-# If only 1 application is found
-else
-{
-    uninstall($Software)
 }
