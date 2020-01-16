90f604fb1fe584cb89cfc65ddf4c17f3e39c6cc6
Fifteen 15 Studios <havens1515@gmail.com>
2020-01-16 10:13:26 -0500

    Update Remove-Software.ps1

--- Client Management/Remove-Software.ps1
+++ Client Management/Remove-Software.ps1
@@ -1,4 +1,18 @@
-﻿param(
+###############################################################################
+#
+# This script does a WMI query for software with a given name and attempts to 
+#   remove the software with MSIExec. If there are multiple software packages
+#   discovered in the search, it will prompt the user for each piece of 
+#   software, asking if the user would like to remove the package.
+#
+# Input - SoftwareToFind (Required) - A string to search for. This input will
+#   be used to search for software which has a name matching this value. A
+#   wildcard is added to each side of the input value when the search is 
+#   performed.
+#
+###############################################################################
+
+param(
     [Parameter(Mandatory=$True)]
     $SoftwareToFind
 )
@@ -20,7 +34,6 @@ function uninstall($App)
     }
 
 }
-
 
 # If there is no macth found
 if($Software -eq $null)
