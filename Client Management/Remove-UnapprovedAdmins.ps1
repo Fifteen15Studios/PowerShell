cabf6cd09dcd778fad2fc8b6240f5fa518ffe42b
Fifteen 15 Studios <havens1515@gmail.com>
2020-03-10 14:27:10 -0400

    Update Remove-UnapprovedAdmins.ps1

--- Client Management/Remove-UnapprovedAdmins.ps1
+++ Client Management/Remove-UnapprovedAdmins.ps1
@@ -1,29 +1,43 @@
-#Purpose:  Removes unauthorized user(s) from local administrator group
+##############################################################################
 #
+# Purpose:  Removes unauthorized user(s) from local administrator group
+#
+###############################################################################
+
 #Requires -runasadmin
 
-# Add other values here, each entryseparated by a coma
-$Approved = "Administrator","Domain Admins" 
+# Add other values here, each entry separated by a comma
+$Approved = "Administrator","Domain Admins"
 
-#Get group object
+# Get administrators group object
 $obj_group = [ADSI]"WinNT://localhost/Administrators,group"
 
-#Get members of group
+# Get members of group
 $Members= @($obj_group.psbase.Invoke("Members")) | foreach{(([ADSI]$_).InvokeGet("Name"))}
+$Count = 0
 
+# Iterate through all users in the administrators group
 $Members | foreach{
  
-    #Check to see if any unacceptable users are in the group. If so, remove them.
+    # Check to see if any unacceptable users are in the group. If so, remove them.
     if(-not ($Approved -contains $_))
     {
+        $count++
+
         try
         {
             $obj_group.Remove("WinNT://$_")
-            Write-Host "Removed ""$_"" from ""Administrators"" group" -ForegroundColor Green
+            Write-Host "Removed ""$_"" from ""Administrators"" group on $env:COMPUTERNAME" -ForegroundColor Green
         }        
         catch
         {
-            Write-Host "Failed to remove ""$_"" from ""Administrators"" group" -ForegroundColor Red
+            Write-Host "Failed to remove ""$_"" from ""Administrators"" group on $env:COMPUTERNAME" -ForegroundColor Red
         }
     }
+}
+
+# Output if nothing was found
+if($Count -eq 0)
+{
+    Write-Host "No Unathorized administrators found on $env:COMPUTERNAME!" -ForegroundColor Green
 }
