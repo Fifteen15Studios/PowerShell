ddbc997cd31ddb35567c2883c1a98bf67aec7e3c
Fifteen 15 Studios <havens1515@gmail.com>
2020-12-15 16:47:27 -0500

    Add files via upload

---
+++ Tools/Get-Servers.ps1
@@ -0,0 +1,13 @@
+﻿###############################################################################
+#
+# Gets all PCs in a domain that are running a server OS. The last line 
+#   optionally exports the list to a CSV. To do so, uncomment this line and
+#   enter a file path in place of <path>
+#
+###############################################################################
+
+Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address 
+# | Export-Csv "<path>\Servers.csv" -NoTypeInformation -Force
