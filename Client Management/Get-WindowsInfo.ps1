75fc3580942dd53b4b20e779467b83158562faf8
Fifteen 15 Studios <havens1515@gmail.com>
2020-12-18 15:51:57 -0500

    Allow for remote use without PS Remoting or WinRM enabled

--- Client Management/Get-WindowsInfo.ps1
+++ Client Management/Get-WindowsInfo.ps1
@@ -2,23 +2,94 @@
 #
 # This script gets a bunch of information about the current install of Windows
 #
+# This should work in any environment, as it does not require 
+#    PowerShell remoting or any other specific environment setup to reach the
+#    remote PC. It simply uses WMI to do all of its work on remote machines.
+#
 ###############################################################################
 
+$ComputerName = Read-Host "Enter a computer name (Leave blank for local machine)"
+
 # Get Information
-$Name = (Get-CimInstance Win32_OperatingSystem).caption
-$ServerOS = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").InstallationType -eq "Server"
-$Bit = (Get-CimInstance Win32_OperatingSystem).OSArchitecture
-$Version = [Version](Get-CimInstance win32_operatingsystem).version
-$Build= (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentBuild
-$InstallDate = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970')).AddSeconds($(Get-ItemProperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion").InstallDate)
+function Get-Info($ComputerName) {
 
-# If Windows 10, get Release ID
-if($Version.Major -ge 10){$ReleaseID=(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId}
+    # If remote computer
+    if($ComputerName -ne $env:COMPUTERNAME){     
 
-function getUptime
-{
-    $uptime = ((get-date) - (Get-CimInstance Win32_OperatingSystem).LastBootUpTime)
+        # Get-CimInstance doesn't work remotely for some reason - unless firewall is open and other items are in place
+        # It also formats LastBootTime differently than Get-WmiObject
+        $OS = Get-WmiObject -ComputerName $ComputerName Win32_OperatingSystem
 
+        $Version = [Version]$OS.version
+
+        # Registry variables - needed because of WMI remote registry method
+        #$HKEY_CURRENT_USER = 2147483649
+        $HKEY_Local_Machine =2147483650
+        $Reg = [WMIClass]"\\$ComputerName\ROOT\DEFAULT:StdRegProv"
+        $Key = "SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+
+        # Keys we need to get
+        $ServerValue = "InstallationType"
+        $BuildValue = "CurrentBuild"
+        $InstallDateValue = "InstallDate"
+        $ReleaseIDValue = "ReleaseId"
+
+        # Read from remote registry, then get results from the read
+        $Results = $Reg.GetStringValue($HKEY_Local_Machine, $Key, $ServerValue)
+        $ServerOS = $results.sValue -eq "Server"
+
+        $Results = $Reg.GetStringValue($HKEY_Local_Machine, $Key, $BuildValue)
+        $Build = $results.sValue
+
+        # Format the install date properly
+        $InstallDate = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970')).AddSeconds($($Reg.GetDWORDValue($HKEY_Local_Machine, $Key, $InstallDateValue).uValue))
+        
+        # If Windows 10, get ReleaseID
+        if($Version.Major -ge 10){
+            $Results = $Reg.GetStringValue($HKEY_Local_Machine, $Key, $ReleaseIDValue)
+            $ReleaseID = $Results.sValue
+        }
+    }
+    # If local computer
+    else {
+        # Do not change from Get-WmiObject to Get-CimInstance. LastBootTime is formatted diferently in the 2 functions.
+        $OS = Get-WmiObject Win32_OperatingSystem
+
+        $Version = [Version]$OS.version
+
+        # Get values from registry
+        $ServerOS = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").InstallationType -eq "Server"
+        $Build = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentBuild
+        $InstallDate = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970')).AddSeconds($(Get-ItemProperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion").InstallDate)
+
+        # If Windows 10, get Release ID
+        if($Version.Major -ge 10){
+            $ReleaseID=(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
+        }
+    }
+
+    # These are retrieved the same way whether local or remote
+    $Name = $OS.caption
+    $Bit = $OS.OSArchitecture
+    $LastBootUpTime = $OS.ConvertToDateTime($OS.LastBootUpTime)
+
+    # Put Information into an object
+    $Info = New-Object -TypeName psobject
+
+    $Info | Add-Member -MemberType NoteProperty -Name Name -Value $Name
+    $Info | Add-Member -MemberType NoteProperty -Name Bits -Value $Bit
+    $Info | Add-Member -MemberType NoteProperty -Name Server -Value $ServerOS
+    $Info | Add-Member -MemberType NoteProperty -Name Version -Value $Version
+    # If Windows 10, Add Release ID
+    if($Version.Major -ge 10){$Info | Add-Member -MemberType NoteProperty -Name ReleaseID -Value $ReleaseID}
+    $Info | Add-Member -MemberType NoteProperty -Name Build -Value $Build
+    $Info | Add-Member -MemberType NoteProperty -Name InstallDate -Value $InstallDate
+
+    # Calculate uptime based on current time and last boot time
+    $uptime = ((get-date) - $LastBootUpTime)
+
+
+    # Format uptime for display
     $Output = ""
 
     if($uptime.Days -gt 1)
@@ -41,21 +112,15 @@ function getUptime
     elseif($uptime.Seconds -eq 1)
     {$Output += "1 Second "}
 
-    "$($Output.trim())"
+    $Info | Add-Member -MemberType NoteProperty -Name UpTime -Value ($($Output.trim()))
+
+    # Display information
+    $Info
 }
 
-# Put Information into an object
-$Info = New-Object -TypeName psobject
+# If computername is empty, use local computer
+if(-not [bool]$ComputerName) {
+    $ComputerName = $env:COMPUTERNAME
+}
 
-$Info | Add-Member -MemberType NoteProperty -Name Name -Value $Name
-$Info | Add-Member -MemberType NoteProperty -Name Bits -Value $Bit
-$Info | Add-Member -MemberType NoteProperty -Name Server -Value $ServerOS
-$Info | Add-Member -MemberType NoteProperty -Name Version -Value $Version
-# If Windows 10, Add Release ID
-if($Version.Major -ge 10){$Info | Add-Member -MemberType NoteProperty -Name ReleaseID -Value $ReleaseID}
-$Info | Add-Member -MemberType NoteProperty -Name Build -Value $Build
-$Info | Add-Member -MemberType NoteProperty -Name InstallDate -Value $InstallDate
-$Info | Add-Member -MemberType NoteProperty -Name UpTime -Value (getUptime)
-
-# Display information
-$Info
+Get-Info($ComputerName)
