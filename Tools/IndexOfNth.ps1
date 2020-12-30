81ac37ea064ee4fc675ac24ab6bfd4ee1919b934
Fifteen 15 Studios <havens1515@gmail.com>
2020-12-30 14:16:17 -0500

    Update IndexOfNth.ps1

--- Tools/IndexOfNth.ps1
+++ Tools/IndexOfNth.ps1
@@ -5,31 +5,23 @@
 # string. 
 # 
 # Example: if you are looking for the 3rd e in the string "The bee's knees"
-# it would be used as such: indexOfNth("The bee's knees", "e", 3)
+# it would be used as such: indexOfNth "The bee's knees" "e" 3
 ##########################################################################
 
-function indexOfNth([string]$string, [string]$find, [int]$N)
+function indexOfNth([string]$string, [string]$find, [int]$N=1, [int]$startIndex=0)
 {
-    #if index doesn't exist, initialize it to 0
-    try
-    {
-        Get-Variable index -ErrorAction silentlyContinue | Out-Null
-    }
-    Catch
-    {$index = 0}
+    #find next index
+    $index = $string.indexOf($find,$startIndex)
 
     #If it's not the last run
     if($N -ne 1)
     {
         #pass the next index into the function
-        $index = $string.indexOf($find,$index)+1
-        indexofNth $string $find ($N-1)
+        indexofNth $string $find ($N-1) ($index+1)
     }
     #If last run
     else
     {
-        #find last index
-        $index = $string.indexOf($find,$index)
         #Return result
         $index
         Remove-Variable index
