89a2ba1b856157a546f1c25f24bc23957ddcb8b4
Fifteen 15 Studios <havens1515@gmail.com>
2021-01-18 14:01:46 -0500

    Update Template.ps1

--- Template.ps1
+++ Template.ps1
@@ -3,20 +3,22 @@ Function Get-Something {
 .SYNOPSIS
     This is a basic overview of what the script is used for..
  
+ .DESCRIPTION
+    Describes in more detail what you're doing
  
+.LINK
+    https://thesysadminchannel.com/powershell-template -
+    
+# The 2 below won't actually show in Get-Help
+
 .NOTES
     Name: Get-Something
     Author: theSysadminChannel
     Version: 1.0
     DateCreated: 2020-Dec-10
  
- 
 .EXAMPLE
     Get-Something -UserPrincipalName "username@thesysadminchannel.com"
- 
- 
-.LINK
-    https://thesysadminchannel.com/powershell-template -
 #>
  
     [CmdletBinding()]
