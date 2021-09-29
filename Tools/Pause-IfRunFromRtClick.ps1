c751e9b7506b89af029e6b4e478850592cf3f032
Fifteen 15 Studios <havens1515@gmail.com>
2021-09-29 10:04:43 -0400

    Add files via upload

---
+++ Tools/Pause-IfRunFromRtClick.ps1
@@ -0,0 +1,7 @@
+﻿# If user used "Run with Powershell" command from right-click menu
+$Command = (((get-itemproperty -literalpath 'HKLM:\SOFTWARE\Classes\SystemFileAssociations\.ps1\Shell\0\Command').'(default)').split('"').trim() | where {$_})[2] -replace " '%1'",""
+
+if($MyInvocation.line.StartsWith($command))
+{
+    Start-Sleep 30
+}
