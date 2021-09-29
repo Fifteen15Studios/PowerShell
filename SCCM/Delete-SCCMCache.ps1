5713e01c52664ecc3e4dcbb5e68102a884344351
Fifteen 15 Studios <havens1515@gmail.com>
2021-09-29 09:57:31 -0400

    Add files via upload

---
+++ SCCM/Delete-SCCMCache.ps1
@@ -0,0 +1,11 @@
+﻿$resman = new-object -com "UIResource.UIResourceMgr"
+$cacheInfo = $resman.GetCacheInfo()
+
+$cacheinfo.GetCacheElements()  |
+foreach
+{
+    #$_.ContentID is packageID
+    If ($_.ContentSize -gt 0.00) {
+        $cacheInfo.DeleteCacheElement($_.CacheElementID)
+    }
+}
---
+++ SCCM/Set-SCCMCacheSize.ps1
@@ -0,0 +1,10 @@
+﻿$NewCacheSize = 51200
+$Cache = Get-WmiObject -Namespace 'ROOT\CCM\SoftMgmtAgent' -Class CacheConfig
+
+if($Cache.size -lt $NewCacheSize){
+    $Cache.Size = $NewCacheSize
+    $Cache.Put() | Out-Null
+}
+
+# Optional - Forces the change to happen immediately.
+#Restart-Service -Name CcmExec
